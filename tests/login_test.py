5ab0c48f5e133f9dd1d053125a040eeed1007a2e
karol_orlowicz <karol.orlowicz@gmail.com>
2021-10-11 23:36:33 +0200

    pyhamcrest added

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,4 +1,5 @@
 import pytest
+from hamcrest import assert_that, equal_to
 
 from credentials.credentials import PassGitHub
 from pages.github_repo_page import GitHubRepoPage
@@ -14,7 +15,7 @@ class TestLogIn:
         login_page.input_login(PassGitHub.EMAIL) \
             .input_password(PassGitHub.PASSWORD) \
             .click_sign_in_button()
-        assert github_repo_page.is_repo_list_container_visible()
+        assert_that(github_repo_page.is_repo_list_container_visible(), equal_to(True))
 
     def test_should_sign_in_github_account_with_username(self, browser):
         login_page = LoginPage(browser)
@@ -22,18 +23,18 @@ class TestLogIn:
         login_page.input_login(PassGitHub.USERNAME) \
             .input_password(PassGitHub.PASSWORD) \
             .click_sign_in_button()
-        assert github_repo_page.is_repo_list_container_visible()
+        assert_that(github_repo_page.is_repo_list_container_visible(), equal_to(True))
 
     def test_should_not_sign_in_with_incorrect_password(self, browser):
         login_page = LoginPage(browser)
         login_page.input_login(PassGitHub.EMAIL) \
             .input_password("IncorrectPassword") \
             .click_sign_in_button()
-        assert login_page.is_login_in_error_displayed()
+        assert_that(login_page.is_login_in_error_displayed(), equal_to(True))
 
     def test_should_not_sign_in_with_incorrect_login(self, browser):
         login_page = LoginPage(browser)
         login_page.input_login("fake_user_12340000000000001234") \
             .input_password("IncorrectPassword") \
             .click_sign_in_button()
-        assert login_page.is_login_in_error_displayed()
+        assert_that(login_page.is_login_in_error_displayed(), equal_to(True))
