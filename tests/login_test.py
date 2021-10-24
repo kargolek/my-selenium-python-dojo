3cf29be91494d908cb78c23fbff3088ac91507a6
karol_orlowicz <karol.orlowicz@gmail.com>
2021-10-24 23:27:11 +0200

    tests

--- tests/login_test.py
+++ tests/login_test.py
@@ -11,18 +11,21 @@ class TestGitHubLogIn:
         github_login_page.input_login(Secrets.EMAIL) \
             .input_password("MyTestSeleniumPython001") \
             .click_sign_in_button()
+        print(web_driver_each.current_url)
         assert_that(github_repo_page.is_repo_list_container_visible(), equal_to(True))
 
     def test_should_sign_in_github_account_with_username(self, web_driver_each, github_login_page, github_repo_page):
         github_login_page.input_login(Secrets.USERNAME) \
             .input_password("MyTestSeleniumPython001") \
             .click_sign_in_button()
+        print(web_driver_each.current_url)
         assert_that(github_repo_page.is_repo_list_container_visible(), equal_to(True))
 
     def test_should_not_sign_in_with_incorrect_password(self, web_driver_each, github_login_page):
         github_login_page.input_login(Secrets.EMAIL) \
             .input_password("IncorrectPassword") \
             .click_sign_in_button()
+        print(web_driver_each.current_url)
         assert_that(github_login_page.is_login_in_error_displayed(), equal_to(True))
 
     def test_should_not_sign_in_with_incorrect_login(self, web_driver_each, github_login_page):
@@ -30,4 +33,5 @@ class TestGitHubLogIn:
             .click_sign_in_button()
         github_login_page.input_password("IncorrectPassword") \
             .click_sign_in_button()
+        print(web_driver_each.current_url)
         assert_that(github_login_page.is_login_in_error_displayed(), equal_to(True))
