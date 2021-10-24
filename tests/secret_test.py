80d68b14b49afaf553f1ffb38ec1b226882e5bce
karol_orlowicz <karol.orlowicz@gmail.com>
2021-10-24 18:15:25 +0200

    checks

---
+++ tests/secret_test.py
@@ -0,0 +1,10 @@
+import os
+
+from hamcrest import assert_that, equal_to
+
+
+class TestSecret:
+
+    def test_secret(self, web_driver_each, web_driver_each_quit):
+        print(os.environ)
+        assert_that(os.environ.get("EMAIL_GIT_TEST"), equal_to("example@gmail.com"))
