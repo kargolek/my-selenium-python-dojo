a1cc5e81c215cf8aca2a53b38e346f612011b9d6
karol_orlowicz <karol.orlowicz@gmail.com>
2021-10-25 02:48:30 +0200

    tests

--- tests/sign_in_test.py
+++ tests/sign_in_test.py
@@ -1,11 +1,7 @@
 import time
 
 import pytest
-from hamcrest import assert_that, equal_to
 
-from credentials.secrets import Secrets
-from pages.github_device_verification_page import GitHubDeviceVerificationPage
-from pages.gmail.gmail_about_page import GmailAboutPage
 from utilities.mail import Mail
 
 
@@ -14,5 +10,5 @@ class TestGitHubLogIn:
 
     def test_should_sign_in_github_account_with_email(self, web_driver_each, github_login_page, github_repo_page):
         Mail().read_email_from_gmail()
+        time.sleep(10)
         pass
-
--- utilities/mail.py
+++ utilities/mail.py
@@ -1,6 +1,5 @@
 import email
 import imaplib
-import traceback
 
 from credentials.secrets import Secrets
 
@@ -18,29 +17,24 @@ class Mail:
             return msg.get_payload(None, True)
 
     def read_email_from_gmail(self):
-        try:
-            mail = imaplib.IMAP4_SSL(self.SMTP_SERVER)
-            mail.login(self.FROM_EMAIL, self.FROM_PWD)
-            mail.select('inbox')
+        mail = imaplib.IMAP4_SSL(self.SMTP_SERVER)
+        mail.login(self.FROM_EMAIL, self.FROM_PWD)
+        mail.select('inbox')
 
-            data = mail.search(None, 'ALL')
-            mail_ids = data[1]
-            id_list = mail_ids[0].split()
-            first_email_id = int(id_list[0])
-            latest_email_id = int(id_list[-1])
+        data = mail.search(None, 'ALL')
+        mail_ids = data[1]
+        id_list = mail_ids[0].split()
+        first_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
 
-            for i in range(latest_email_id, first_email_id, -1):
-                data = mail.fetch(str(i), '(RFC822)')
-                for response_part in data:
-                    arr = response_part[0]
-                    if isinstance(arr, tuple):
-                        msg = email.message_from_string(str(arr[1], 'utf-8'))
-                        email_subject = msg['subject']
-                        email_from = msg['from']
-                        print('From : ' + email_from + '\n')
-                        print('Subject : ' + email_subject + '\n')
-                        print(self.get_body(msg))
-
-        except Exception as e:
-            traceback.print_exc()
-            print(str(e))
+        for i in range(latest_email_id, first_email_id, -1):
+            data = mail.fetch(str(i), '(RFC822)')
+            for response_part in data:
+                arr = response_part[0]
+                if isinstance(arr, tuple):
+                    msg = email.message_from_string(str(arr[1], 'utf-8'))
+                    email_subject = msg['subject']
+                    email_from = msg['from']
+                    print('From : ' + email_from + '\n')
+                    print('Subject : ' + email_subject + '\n')
+                    print(f"Body: {self.get_body(msg)}\n")
