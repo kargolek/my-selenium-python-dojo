01e7bd4e2798668070e33a5e9375c148379bc5e8
karol_orlowicz <karol.orlowicz@gmail.com>
2021-10-25 02:56:43 +0200

    tests

--- tests/sign_in_test.py
+++ tests/sign_in_test.py
@@ -2,6 +2,7 @@ import time
 
 import pytest
 
+from credentials.secrets import Secrets
 from utilities.mail import Mail
 
 
@@ -9,7 +10,10 @@ from utilities.mail import Mail
 class TestGitHubLogIn:
 
     def test_should_sign_in_github_account_with_email(self, web_driver_each, github_login_page, github_repo_page):
+        github_login_page.input_login(Secrets.EMAIL) \
+            .input_password(Secrets.PASSWORD) \
+            .click_sign_in_button()
+        time.sleep(30)
         Mail().read_email_from_gmail()
-        time.sleep(10)
         assert False
         pass
