80563862d412d4d45d92cfca6b86a6503c6f001c
karol_orlowicz <karol.orlowicz@gmail.com>
2021-11-02 01:57:02 +0100

    fix

--- pages/github_pages/github_device_verification_page.py
+++ pages/github_pages/github_device_verification_page.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
@@ -8,6 +10,8 @@ from utilities.otp_handles.github_otp import GitHubOtp
 class GitHubDeviceVerificationPage(BasePage):
     VERIFICATION_CODE_INPUT = (By.ID, "otp")
     VERIFY_BUTTON = (By.XPATH, "//button[@type='submit']")
+    INCORRECT_CODE = (By.XPATH, "//div[@id='js-flash-container']")
+    RESEND_BUTTON = (By.XPATH, "//*[contains(text(), 'Re-send the code')]")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -28,5 +32,11 @@ class GitHubDeviceVerificationPage(BasePage):
             code = GitHubOtp().get_latest_opt_code()
             print(f"VERIFICATION CODE: {code}")
             self.input_device_code(code)
+            if self._is_element_located_after_wait(self.INCORRECT_CODE, 5):
+                self._wait_for_visible_element(self.RESEND_BUTTON, 5).click()
+                time.sleep(10)
+                code2 = GitHubOtp().get_latest_opt_code()
+                print(f"VERIFICATION CODE: {code2}")
+                self.input_device_code(code2)
             # self.click_verification_device()
             return GitHubDashboardPage(self.driver)
