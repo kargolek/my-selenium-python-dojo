f48954b54a3ec9eb41cd97476ba61e8b6ea87d6b
karol_orlowicz <karol.orlowicz@gmail.com>
2021-11-02 12:27:40 +0100

    fix

--- tests/login_test.py
+++ tests/login_test.py
@@ -11,7 +11,7 @@ class TestGitHubLogIn:
 
     def test_should_sign_in_github_account_with_email(self, web_driver_each, github_login_page, github_repo_page,
                                                       github_otp_page):
-        print(f"DATE TIME TO CHECK ON CI:{datetime.datetime.now()}")
+        print(f"DATE TIME TO CHECK ON CI:{datetime.datetime.utcnow()}")
         github_login_page.input_login(Secrets.EMAIL) \
             .input_password(Secrets.PASSWORD) \
             .click_sign_in_button()
