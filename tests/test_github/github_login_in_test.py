de2b278dd188927a0cd84a5de7c3838245a2183f
karol_orlowicz <karol.orlowicz@gmail.com>
2021-12-07 04:38:10 +0100

    add negative login scenarios

--- tests/test_github/github_login_in_test.py
+++ tests/test_github/github_login_in_test.py
@@ -1,12 +1,14 @@
+import pytest
 import pytest_check as check
 
 from utilities.credentials.secrets import Secrets
 
 
+@pytest.mark.usefixtures("web_driver_each")
 class TestGitHubLoginIn:
 
-    def test_login_in_username_positive(self, web_driver_each, github_login_page_each, driver_utils_each,
-                                        get_current_date_time, github_otp_page_each):
+    def test_login_in_username_positive(self, github_login_page_each, driver_utils_each, get_current_date_time,
+                                        github_otp_page_each):
         github_login_page_each.open_url() \
             .sign_in_github_account(Secrets.USERNAME, Secrets.PASSWORD)
         github_otp_page_each.input_otp_code_if_verification_present(get_current_date_time)
@@ -15,8 +17,8 @@ class TestGitHubLoginIn:
         check.equal(driver_utils_each.get_cookie_value("dotcom_user"), Secrets.USERNAME)
         check.equal(driver_utils_each.get_cookie_value("logged_in"), "yes")
 
-    def test_login_in_email_positive(self, web_driver_each, github_login_page_each, driver_utils_each,
-                                     get_current_date_time, github_otp_page_each):
+    def test_login_in_email_positive(self, github_login_page_each, driver_utils_each, get_current_date_time,
+                                     github_otp_page_each):
         github_login_page_each.open_url() \
             .sign_in_github_account(Secrets.EMAIL, Secrets.PASSWORD)
         github_otp_page_each.input_otp_code_if_verification_present(get_current_date_time)
@@ -24,3 +26,40 @@ class TestGitHubLoginIn:
         check.is_true(driver_utils_each.get_cookie_value("user_session") is not None)
         check.equal(driver_utils_each.get_cookie_value("dotcom_user"), Secrets.USERNAME)
         check.equal(driver_utils_each.get_cookie_value("logged_in"), "yes")
+
+    def test_login_in_username_negative_incorrect_password(self, github_login_page_each, driver_utils_each):
+        github_login_page_each.open_url() \
+            .sign_in_github_account(Secrets.USERNAME, "incorrect password")
+
+        check.is_true(github_login_page_each.is_login_in_error_displayed())
+        check.equal(driver_utils_each.get_cookie_value("user_session"), None)
+        check.equal(driver_utils_each.get_cookie_value("dotcom_user"), None)
+        check.equal(driver_utils_each.get_cookie_value("logged_in"), "no")
+
+    def test_login_in_negative_incorrect_email(self, web_driver_each, github_login_page_each, driver_utils_each):
+        github_login_page_each.open_url() \
+            .sign_in_github_account(Secrets.EMAIL.__add__("123456789"), Secrets.PASSWORD)
+
+        check.is_true(github_login_page_each.is_login_in_error_displayed())
+        check.equal(driver_utils_each.get_cookie_value("user_session"), None)
+        check.equal(driver_utils_each.get_cookie_value("dotcom_user"), None)
+        check.equal(driver_utils_each.get_cookie_value("logged_in"), "no")
+
+    def test_login_in_negative_incorrect_username(self, web_driver_each, github_login_page_each, driver_utils_each):
+        github_login_page_each.open_url() \
+            .sign_in_github_account(Secrets.USERNAME.__add__("12345678"), Secrets.PASSWORD)
+
+        check.is_true(github_login_page_each.is_login_in_error_displayed())
+        check.equal(driver_utils_each.get_cookie_value("user_session"), None)
+        check.equal(driver_utils_each.get_cookie_value("dotcom_user"), None)
+        check.equal(driver_utils_each.get_cookie_value("logged_in"), "no")
+
+    def test_login_in_email_negative_incorrect_password(self, web_driver_each, github_login_page_each,
+                                                        driver_utils_each):
+        github_login_page_each.open_url() \
+            .sign_in_github_account(Secrets.EMAIL, "incorrect password")
+
+        check.is_true(github_login_page_each.is_login_in_error_displayed())
+        check.equal(driver_utils_each.get_cookie_value("user_session"), None)
+        check.equal(driver_utils_each.get_cookie_value("dotcom_user"), None)
+        check.equal(driver_utils_each.get_cookie_value("logged_in"), "no")
